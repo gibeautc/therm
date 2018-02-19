ba1e38a22bf38f56db1cd0745de8c933533eb750
Chadg <gibeautc@oregonstate.edu>
2018-02-18 22:50:39 -0800

    add gui

---
+++ gui.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+import psutil
+import subprocess
+import socket
+from appJar import gui
+import os
+import sys
+
+if os.path.isdir("/home/pi"):
+	system="pi"
+else:
+	system="chadg"
+
+toGuiPath="/home/"+system+"/pipeToGui"
+fromGuiPath="/home/"+system+"/pipeFromGui"
+
+try:	
+	os.mkfifo(toGuiPath)
+except:
+	pass
+#if not os.path.isfile(fromGuiPath):
+try:
+	os.mkfifo(fromGuiPath)
+except:
+	pass
+pipeIn=os.open(toGuiPath,os.O_RDONLY|os.O_NONBLOCK)
+pipeOut=os.open(fromGuiPath,os.O_RDWR)
+
+msgBuffer=[]
+
+
+tools=["UPDATE","CLOSE","OFF"]
+
+def tbFunc(button):
+	print("ToolBar Button "+str(button)+" was pressed")
+	if button=="CLOSE":
+		#gpsObj.running=False
+		exit()
+	if button=='GPS':
+		app.showSubWindow("gpsWindow")
+	if button=="OFF":
+		shutdown=app.yesNoBox("Shutdown","Do you really want to shut down the system?",parent=None)
+		if shutdown:
+			#should probably shutdown other processes?
+			#will need to include a way for them to know they should shutdown
+			#so that MySQL connections and other stuff is closed?
+			subprocess.call("shutdown -H now",shell=True)
+		return 
+	if button=="UPDATE":
+		app.thread(gitPull)
+		
+
+def haveInternet(host="8.8.8.8", port=53, timeout=3):
+	"""
+	Host: 8.8.8.8 (google-public-dns-a.google.com)
+	OpenPort: 53/tcp
+	Service: domain (DNS/TCP)
+	"""
+	try:
+		socket.setdefaulttimeout(timeout)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+		return True
+	except Exception as ex:
+		print ex.message
+		return False
+
+	
+
+def checkUpdate():
+	global msgBuffer
+	msg=""
+	#print("Checking for updates")
+	try:
+		line=os.read(pipeIn,10)
+		#print(line)
+		msgBuffer.append(line)
+		#print(msgBuffer)
+		total="".join(msgBuffer)
+		lines=total.split("\n")
+		if len(lines)<2:
+			return
+		for x in range(len(lines)-1):
+			print("Message: "+lines[x])
+			msg=lines[x]
+		msgBuffer=[]
+		msgBuffer.append(lines[len(lines)-1])
+		#print("Here")	
+		
+	except:
+		#print(sys.exc_info())
+		#print("Error reading from pipe, maybe empty")
+		return
+	#print(line)
+	
+
+
+
+
+def appSetup():
+	if system=="pi":
+		app.setGeometry("fullscreen")
+	else:
+		app.setSize(800,480)
+	app.setTitle("Office Pi")
+	#Tool BAR setup
+	app.addToolbar(tools,tbFunc,findIcon=True)
+	
+	
+	#Status Bar Setup
+	app.addStatusbar(fields=4)
+	app.setStatusbarWidth(4,3)
+	app.setStatusbarBg("red",3)
+	app.setStatusbar("GPS",3)
+	
+	#Main Tabbed Frame
+	app.startTabbedFrame("Main")
+	
+	app.startTab("Office")
+	app.addLabel("OutSidelb","OutSide Temp:",0,0)
+	app.addLabel("Insidelb","Inside Temp:",1,0)
+	app.addLabel("SetPointlb","Set Point:",2,0)
+	app.addLabel("HeatSinklb","Heat Sink Temp:",3,0)
+	app.addLabel("lbOt","00.000",0,1)
+	app.addLabel("lbIt","00.000",1,1)
+	app.addLabel("lbSp","00.000",2,1)
+	app.addLabel("lbHs","00.000",3,1)
+	app.stopTab()
+		
+	app.stopTabbedFrame()
+	
+	#Registered Events
+	app.registerEvent(checkUpdate)
+
+
+app=gui()
+if __name__=="__main__":
+	appSetup()
+#Launch Gui
+	app.go()
+
